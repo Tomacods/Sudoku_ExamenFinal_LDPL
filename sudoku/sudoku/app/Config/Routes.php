a8d56aa24059f4ddfb1ed65b50ebb5b3800e589e
Tomas <tomasds15@gmail.com>
2025-11-17 05:43:04 -0300

    ca

--- sudoku/sudoku/app/Config/Routes.php
+++ sudoku/sudoku/app/Config/Routes.php
@@ -6,6 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-$routes->get('/registro', 'UserController::registro_show');
-$routes->post('/registro', 'UserController::registro_save');
