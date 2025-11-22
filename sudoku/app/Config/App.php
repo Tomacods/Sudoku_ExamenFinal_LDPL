1650ef33bdfb672d3ac2c24a97edcd9a432eb53b
Tomás <Tomasds15@gmail.com>
2025-11-22 08:42:27 -0300

    Update App.php

--- sudoku/app/Config/App.php
+++ sudoku/app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/trabajos/Sudoku_ExamenFinal_LDPL/sudoku/public/';
+    public string $baseURL = 'http://localhost/Sudoku_ExamenFinal_LDPL/sudoku/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
